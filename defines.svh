80808039bab2bc459602fbabb563c587ddac0577
DebangshuKundu <debangshurnjkundu@gmail.com>
2026-07-12 23:19:53 +0530

    Create defines.svh

---
+++ defines.svh
@@ -0,0 +1,13 @@
+`define DATA_DEPTH 256
+`define DATA_WIDTH 32
+`define num_transactions 1000
+/*function integer log2;
+input integer n;
+begin
+log2=0;
+while(2**log2<n) begin
+				log2=log2+1;
+				end
+end
+endfunction*/
+`define ADDR_WIDTH 9
